2fb787f634d3b20c7c500ca71c47314cf9915638
pwang86 <pwang86@gmail.com>
2019-11-17 18:22:00 +1100

    fix animation issue

---
+++ App.js
@@ -0,0 +1,52 @@
+/* // Demo 1. PureComponent
+import React from "react";
+import PropTypes from "prop-types";
+
+class Greeting extends React.PureComponent {
+  render() {
+    return <h1>Hi there, my name is {this.props.name}!</h1>;
+  }
+}
+
+Greeting.propTypes = {
+  name: PropTypes.string
+};
+
+class App extends React.Component {
+  state = {
+    greeted: false,
+    name: "Joni"
+  };
+
+  componentDidMount() {
+    this.setState({ greeted: true });
+  }
+
+  render() {
+    return <Greeting name={this.state.name} />;
+  }
+}
+
+export default App; */
+
+// Demo 2. css animation
+import React, { useState } from "react";
+import classNames from "classnames";
+
+import "./styles.css";
+
+const App = () => {
+  const [animate, setAnimate] = useState(false);
+  const handleClick = () => setAnimate(!animate);
+
+  return (
+    <button
+      onClick={handleClick}
+      className={classNames("animate", animate && "grow")}
+    >
+      Grow this link
+    </button>
+  );
+};
+
+export default App;
